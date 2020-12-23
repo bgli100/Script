e77abd1a42ecbf587f60e18c4d0907136508ce41
bgli100 <bgli100@126.com>
2020-12-23 19:50:49 +1100

    Update bilibili-feed.js

--- bilibili-feed.js
+++ bilibili-feed.js
@@ -2,7 +2,7 @@
 remove non-av entry from bilibili feed.
 
 QX:
-https:\/\/app\.bilibili\.com\/x\/v2\/feed\/index url script-response-body bilibili-feed.js
+https:\/\/app\.bilibili\.com\/x\/v2\/feed\/index? url script-response-body bilibili-feed.js
 */
 
 let body = $response.body;
@@ -10,7 +10,7 @@ let obj = JSON.parse(body);
 console.log(obj);
 let i = 0;
 while(i < obj['data']['items'].length) {
-    if(obj['data']['items'][i]['goto'] && obj['data']['items'][i]['goto'] !== 'av') {
+    if(obj['data']['items'][i].hasOwnProperty('goto') && obj['data']['items'][i]['goto'] != 'av') {
         obj['data']['items'] = obj['data']['items'].splice(i, i);
     } else {
         i++;
