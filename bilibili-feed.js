bd62d2d2f3124855c83a00357773b4ee66d758bf
bgli100 <bgli100@126.com>
2020-12-23 19:15:24 +1100

    Create bilibili-feed.js

---
+++ bilibili-feed.js
@@ -0,0 +1,18 @@
+/*
+remove non-av entry from bilibili feed.
+
+QX:
+https:\/\/app\.bilibili\.com\/x\/v2\/feed\/index url script-response-body bilibili-feed.js
+
+let body = $response.body;
+let obj = JSON.parse(body);
+let i = 0;
+while(i < obj['data']['items'].length) {
+    if(obj['data']['items'][i]['goto'] !== 'av') {
+        obj['data']['items'] = obj['data']['items'].splice(i, i);
+    } else {
+        i++;
+    }
+}
+body = JSON.stringify(obj);
+$done({body});
